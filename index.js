d3199dde4310ddc14ef00cb2ae305c7564f91ad8
Nodeclient <41586831+Nodeclient@users.noreply.github.com>
2020-06-02 08:23:33 +0300

    NA2
    1.0.5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = void 0;
-const _default = require("./src/lib/daytime");
+const _default = require("./src/lib/authentication");
 exports.default = _default;
